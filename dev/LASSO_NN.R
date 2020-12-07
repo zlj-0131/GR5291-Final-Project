d15358df08b30ba5cf11dc763b69c74b6864a53e
fairytalekk <70994813+cu20-dk3112@users.noreply.github.com>
2020-12-06 22:25:19 -0500

    improvement

--- dev/LASSO_NN.R
+++ dev/LASSO_NN.R
@@ -6,11 +6,11 @@ library(e1071)
 
 lassoReg = function(train.x, train.y){
   # train LASSO regression
-  cv <- cv.glmnet(x = as.matrix(train.x), y = train.y,
-                  family="gaussian", alpha = 1, intercept=TRUE)
+  cv <- cv.glmnet(x = as.matrix(train.x), y = train.y, nfolds = 50,
+                  family="binomial", alpha = 1, intercept=TRUE)
   bestlambda <- cv$lambda.min
   lasso <- glmnet(x = as.matrix(train.x), y = train.y,
-                  family="gaussian", alpha = 1, intercept=TRUE, lambda = bestlambda)
+                  family="binomial", alpha = 1, intercept=TRUE, lambda = bestlambda)
   return(lasso)
 }
 
@@ -20,13 +20,14 @@ lassoPred = function(model, test.x){
 }
 
 
-nnReg = function(train_df){
+nnReg = function(train_df, hd){
+  set.seed(2020)
   nn.fit = neuralnet(data = train_df,
                      formula = factor(HEALTH) ~ . ,
-                     hidden = c(8,4),
-                     act.fct = "logistic",
-                     threshold = 0.1,
-                     stepmax = 1e06,
+                     hidden = hd,
+                     threshold = 0.02,
+                     stepmax = 1e+04,
+                     algorithm = "rprop+",
                      linear.output = F)
   return(nn.fit)
 }
